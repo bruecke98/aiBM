cc4080d43b2c95c81a9a8b4c79ea835b1913364c
bruecke98 <brueckmann.paul@gmail.com>
2023-04-13 22:07:09 +0200

    start project

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Header />
+
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+      
+    <Footer />
+  </div>
+</template>
+
+
+<script setup lang="ts">
+useHead({
+  link: [
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com'
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap',
+      crossorigin: ''
+    }
+  ]
+})
+</script>
---
+++ components/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+    <header>
+        <h1 class="text-center">Footer</h1>
+        <slot></slot>
+    </header>
+</template>
