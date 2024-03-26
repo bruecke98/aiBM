6ae3ada7846611ef1fb31b8403065fff10764a36
Paul Brückmann <brueckmann.paul@gmail.com>
2024-03-26 22:03:32 +0100

    mobile

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -1,19 +1,17 @@
 <template>
    
-    <div class="grid grid-cols-2 sticky top-2 bg-cyan-800 h-10 p-1 m-2 rounded-md">
+    <div class="grid grid-cols-2 sticky top-2 bg-cyan-800 h-10 p-1 m-2 rounded-md items-center">
         <!-- Logo -->
-            <div>
-                <nuxt-link to="/" class="text-gray-200 text-3xl">
-                    SPELL-Business Model
-                </nuxt-link>
-            </div>
-            <button @click="findCanvas" class="text-xl text-gray-200">
-               
-                Platform 
-                
+        <div class="flex justify-center">
+            <nuxt-link to="/" class="text-gray-200 text-xl md:text-2xl xl:text-3xl -mt-2">
+                <span class="hidden md:block">SPELL-Business Model</span><span class="block md:hidden">SPELL-BM</span>
+            </nuxt-link>
+        </div>
+        <div class="flex justify-end mr-2">
+            <button @click="findCanvas" class="text-sm md:text-2xl xl:text-3xl text-gray-200 -mt-3 mr-1 md:mr-14 xl:mr-28">
+                <img src="../../public/favicon.png" class="w-14 h-14" /> 
             </button>
-                
-           
+        </div>
     </div>
     
 </template>
